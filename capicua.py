54e1ad54246843c252e0e677075c01263704af90
Néstor Jesús Páez Sarmiento <101004349+npaezsar@users.noreply.github.com>
2023-03-11 08:25:09 -0500

    Update capicua.py

--- capicua.py
+++ capicua.py
@@ -1,5 +1,5 @@
 #  QUIZ CONDICIONALES
-"""Leer un número entero de 5 digitos, y verificar si se trata de un número capicúa.
+"""Leer un número entero de 5 digitos, y verificar si se trata de un número capicúa."""
 
 print("-------------------------------------------")
 print("----------- NUMERO CAPICÚA ----------------")
